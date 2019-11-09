356ce0f68fc2d029779c2291ba8efc36a0c458b0
Jakub Kwiatek <kwiatekjakub52@gmail.com>
2019-11-09 14:50:06 +0100

    added scrapper

--- ETL/Webscraper/EntryModel.cs
+++ ETL/Webscraper/EntryModel.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace ETL.Webscraper
 {
-    public class EntryModel
+    public class MovieModel
     {
-
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Director { get; set; }
+        public List<Actor> Actors { get; set; }
+
+        public MovieModel(string cTitle, string cDescription, string cDirector, List<Actor> cActors)
+        {
+            Title = cTitle;
+            Description = cDescription;
+            Director = cDirector;
+            Actors = cActors;
+        }
+
+        override public string ToString(){
+            return Title;
+        }
+    }
+
+    public class Actor
+    {
+        public string Name { get; set; }
+
+        public Actor(string cName)
+        {
+            Name = cName;
+        }
+
+        override public string ToString(){
+            return Name;
+        }
     }
 }
